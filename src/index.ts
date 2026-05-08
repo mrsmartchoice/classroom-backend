eebbb5fe11b1e8bd102d3b1e6d7ee238b8d89da0
Mrsmartchoice <technicaleldnight@gmail.com>
2026-05-08 18:02:04 +0100

    retrying from init  after failed of serverless pg

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,14 @@
-
-import express from "express";
+import express from 'express';
 
 const app = express();
 const PORT = 8000;
 
 app.use(express.json());
 
-
-app.get("/", (req, res) => {
-  res.send("Hello from the TypeScript Express server");
+app.get('/', (req, res) => {
+    res.send('Hello, World!');
 });
 
-
-
 app.listen(PORT, () => {
-  console.log(`Server started on http://localhost:${PORT}`);
+    console.log(`Server is running on http://localhost:${PORT}`);
 });
